a2db72499a5947624d76d22d8c2718c515fb2249
cjoyy <joycalvin1604@gmail.com>
2024-12-09 12:07:55 +0700

    create review

--- lib/main/navbar.dart
+++ lib/main/navbar.dart
@@ -1,8 +1,8 @@
+import 'package:dinepasar_mobile/review/review.dart';
 import 'package:flutter/material.dart';
 import 'package:dinepasar_mobile/search/screens/explore_page.dart';
 import 'package:dinepasar_mobile/profile/screens/profile.dart';
 import 'package:dinepasar_mobile/densiklopedia/densiklopedia.dart';
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -16,12 +16,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // List of pages to navigate between
   final List<Widget> _pages = <Widget>[
-    const Placeholder(),  // Explore Page
-    const ExplorePage(),  // Placeholder for Search Page
-    const Placeholder(),  // Placeholder for Review Page
-    const Placeholder(),  // Placeholder for Favorite Page
+    const Placeholder(), // Explore Page
+    const ExplorePage(), // Placeholder for Search Page
+    const ReviewPage(), // Placeholder for Review Page
+    const Placeholder(), // Placeholder for Favorite Page
     const DensiklopediaPage(), // Placeholder for Favorite Page
-    const ProfilePage(),  // Placeholder for Profile Page
+    const ProfilePage(), // Placeholder for Profile Page
   ];
 
   void _onItemTapped(int index) {
@@ -37,12 +37,15 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Dinepasar'),
         backgroundColor: Color.fromARGB(255, 200, 161, 35),
       ),
-      body: _pages[_selectedIndex],  // Show the selected page from the _pages list
+      body:
+          _pages[_selectedIndex], // Show the selected page from the _pages list
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,  // Set the current selected tab
-        onTap: _onItemTapped,  // Handle tab switch
-        selectedItemColor: Colors.yellow[700],  // Highlight color for the selected item
-        unselectedItemColor: Colors.grey,  // Set the color of unselected items to grey
+        currentIndex: _selectedIndex, // Set the current selected tab
+        onTap: _onItemTapped, // Handle tab switch
+        selectedItemColor:
+            Colors.yellow[700], // Highlight color for the selected item
+        unselectedItemColor:
+            Colors.grey, // Set the color of unselected items to grey
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
