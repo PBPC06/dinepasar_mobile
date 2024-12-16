ece1bd45796cb44761622e044d5757f9170b845d
PBPC06 <pebepec06@gmail.com>
2024-12-16 20:28:32 +0700

    ganti link ke pws

--- lib/main/screens/login.dart
+++ lib/main/screens/login.dart
@@ -100,7 +100,7 @@ class _LoginPageState extends State<LoginPage> {
 
                       // Send the login request
                       final response = await request
-                          .login("http://127.0.0.1:8000/manageData/login_flutter/", {
+                          .login("https://namira-aulia31-dinepasar.pbp.cs.ui.ac.id/manageData/login_flutter", {
                         'username': username,
                         'password': password,
                       });
